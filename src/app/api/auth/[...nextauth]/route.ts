3b423128ca6370e5dcbf090c586084a2ff3ddd22
Abdussamad Bello <abdussamadbello4real@hotmail.co.uk>
2024-10-22 03:08:51 +0100

    Add authentication route and configuration

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,72 @@
+
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/lib/db';
+import bcrypt from 'bcrypt';
+
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password are required');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error('No user found with this email');
+        }
+
+        if (!user.password) {
+          throw new Error('User password is null');
+        }
+        const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
+
+        if (!isPasswordValid) {
+          throw new Error('Invalid password');
+        }
+
+        return { id: user.id, email: user.email, name: user.name };
+      },
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.email = token.email as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error',
+    newUser: '/auth/new-user',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
